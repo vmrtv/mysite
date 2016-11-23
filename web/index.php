176936b182210af27d4736868ff8217f7bb9814a
Marat Satashev <marat010665@mail.ru>
2016-11-23 18:34:44 +0400

    Index file changed

--- web/index.php
+++ web/index.php
@@ -7,6 +7,7 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../config/web.php');	
+$config = require(__DIR__ . '/../config/web.php');
+echo 'Тест 123';
 	//print_r($config);
 (new yii\web\Application($config))->run();
